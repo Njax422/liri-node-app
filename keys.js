a873d951ffd20cc7352bb0e5c4b3c281666179b2
Bootcamp <njackson422@gmail.com>
2017-11-01 22:45:03 -0400

    add four functions to Liri

---
+++ keys.js
@@ -0,0 +1,18 @@
+var twitterKeys = {
+  consumer_key: 'SWZBmMCSf5PeXpHhkfff2o9UV',
+  consumer_secret: 'eIjO57RAfoAd35L2E4dPYDiaUfPycsZtq3BN4nwgJUarxUEAi8',
+  access_token_key: '922192859503190018-o0LRlZvNOXn9CFVsJlIvSEUlTHadc0p',
+  access_token_secret: 'x59VYMQp3d2CIKhYSEFIATpEei4y937TKb3x2PSFDOfdW'
+}
+
+var spotifyKeys = {
+	id: 'e2778959638b4026983a050f21452bb8',
+	secret: 'd4fc78f81af84a4a879a12d5ceee58c2'
+}
+
+module.exports = {
+	twitterKeys,
+	spotifyKeys
+}
+
+// console.log(twitterKeys.consumer_key);
---
+++ liri.js
@@ -0,0 +1,84 @@
+
+var command = process.argv[2];
+var request = require('request');
+var keys = require("./keys.js");
+
+var Twitter = require('twitter');
+var Spotify = require('node-spotify-api');
+
+if (command === "my-tweets") {
+	myTweets();
+} else if (command === "spotify-this-song") {	
+	spotifyThis();
+} 
+else if (command === "movie-this") {
+	movieThis();
+} else if (command == "do-what-it-says") {
+	doWhatItSays();
+}
+
+function myTweets (){
+	var client = new Twitter(keys.twitterKeys);
+	var params = {screen_name: 'e98wruoifdjll'};
+	client.get('statuses/user_timeline', params, function(error, tweets, response) {
+	  	if (!error) {
+	  		for (var i = 0; i < tweets.length; i++) {
+	  		console.log("- " + tweets[i].text);
+	  		}
+	  	} else if (error) {
+	  	console.log(error);
+	  	}
+	});
+}
+
+function spotifyThis (){
+	console.log(keys.spotifyKeys);
+	var spotify = new Spotify(keys.spotifyKeys);
+	var song = "hello";
+	spotify.search({ type: 'track', query: song}, function(err, data) {
+	  	if (err) {
+	    	return console.log('Error occurred: ' + err);
+	  	}	
+		console.log(data);
+	});
+	// This will show the following information about the song in your terminal/bash window
+	// Artist(s)
+	// The song's name
+	// A preview link of the song from Spotify
+	// The album that the song is from
+	// If no song is provided then your program will default to "The Sign" by Ace of Base.
+};
+
+function movieThis (){
+	var movieName = "";
+	var args = process.argv;
+	for (var i = 3; i < args.length; i++) {
+	  if (i > 3) {
+	    movieName = movieName + "+" + args[i];
+	  } else {
+	    movieName += args[i];
+	  }
+	}
+	console.log("Our movie is", movieName);
+	request("http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=40e9cece", function(error, response, body) {
+	  if (!error && response.statusCode === 200) {
+
+	    console.log("The movie's rating is: " + JSON.parse(body).imdbRating);
+	    // This will output the following information to your terminal/bash window:
+		//    * Title of the movie.
+		//    * Year the movie came out.
+		//    * IMDB Rating of the movie.
+		//    * Rotten Tomatoes Rating of the movie.
+		//    * Country where the movie was produced.
+		//    * Language of the movie.
+		//    * Plot of the movie.
+		//    * Actors in the movie.
+	  } 
+	// If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
+	});
+}
+
+function doWhatItSays () {
+// Using the fs Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
+// It should run spotify-this-song for "I Want it That Way," as follows the text in random.txt.
+}
